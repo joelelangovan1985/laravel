82a0e4a51849c852196edd12ac21fa694e618fc3
joelelangovan1985 <joelelangovan1985@users.noreply.github.com>
2015-05-28 21:38:09 +0300

    Create database.php

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'host'      => '52.25.144.133',
+			'database'  => 'forge',
+			'username'  => 'forge',
+			'password'  => 'KFrRFR8yKUe2znJSWbLV',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
